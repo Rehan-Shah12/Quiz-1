1185dd91cb5d3c302b3e5f494a69bcd1a18b7123
Rehan-Shah12 <rehannaqi835@gmail.com>
2022-03-24 11:40:16 +0500

    Value fetched and Displayed Succesfully
    value can be fetched from input fields and displayed on the page

--- bindings.js
+++ bindings.js
@@ -1,12 +1,38 @@
 window.onload = function () {
   var eduSubmit = document.getElementById("eduSubmit");
   eduSubmit.onclick = updateEduTime;
-  document
-    .getElementById("timeYearEdu")
-    .addEventListener("change", updateEduTime);
 };
 function updateEduTime() {
+  var eduDiv = document.getElementById("eduContainer");
+
   var eduTime = document.getElementById("timeYearEdu").value;
-  var eduTimeOutput = document.getElementById("newDateEdu");
-  eduTimeOutput.innerHTML = eduTime.value;
+  var newEduText = document.createTextNode(eduTime);
+  var newButton = document.createElement("button");
+  newButton.className = "btn btn-primary border-radius mt-5";
+
+  newButton.appendChild(newEduText);
+  eduDiv.appendChild(newButton);
+
+  var degree = document.getElementById("degree").value;
+  var newEduDegree = document.createTextNode(degree);
+  var heading = document.createElement("h5");
+  heading.className = "font-weight-bold mt-2";
+
+  heading.appendChild(newEduDegree);
+  eduDiv.appendChild(heading);
+
+  var institute = document.getElementById("institute").value;
+  var newEduInstitute = document.createTextNode(institute);
+  var subEduInstitute = document.createElement("sub");
+
+  subEduInstitute.appendChild(newEduInstitute);
+  eduDiv.appendChild(subEduInstitute);
+
+  var eduDescription = document.getElementById("eduDescription").value;
+  var newEduDescription = document.createTextNode(eduDescription);
+  var pEduDescription = document.createElement("p");
+  pEduDescription.className = "mt-4";
+
+  pEduDescription.appendChild(newEduDescription);
+  eduDiv.appendChild(pEduDescription);
 }
