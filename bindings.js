3b06f63d52489405e37991072661b47bc9d4c4c9
Rehan-Shah12 <rehannaqi835@gmail.com>
2022-03-26 00:23:25 +0500

    Both forms Working with delete button
    Both forms Working with delete button but the hard-coded divs are not being deleted

--- bindings.js
+++ bindings.js
@@ -1,9 +1,12 @@
 window.onload = function () {
+  var expSubmit = document.getElementById("expSubmit");
+  expSubmit.onclick = updateExpTime;
   var eduSubmit = document.getElementById("eduSubmit");
   eduSubmit.onclick = updateEduTime;
 };
 function updateEduTime() {
   var eduDiv = document.getElementById("eduContainer");
+
   var newEduDivCreated = document.createElement("div");
 
   //------------------------------------------------------------
@@ -61,9 +64,71 @@ function updateEduTime() {
   newEduDivCreated.appendChild(deleteButton);
   eduDiv.appendChild(newEduDivCreated);
 }
+//-------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------
+function updateExpTime() {
+  var expDiv = document.getElementById("expContainer");
+  var newExpDivCreated = document.createElement("div");
+
+  var expTime = document.getElementById("timeYearExp").value;
+  var newExpText = document.createTextNode(expTime);
+  var newExpButton = document.createElement("button");
+  newExpButton.className = "btn btn-primary border-radius mt-5";
+
+  newExpButton.appendChild(newExpText);
+  newExpDivCreated.appendChild(newExpButton);
+  expDiv.appendChild(newExpDivCreated);
+
+  //-------------------------------------------------------------------------------------------------------
+  var job = document.getElementById("job").value;
+  var newExpJob = document.createTextNode(job);
+  var heading = document.createElement("h5");
+  heading.className = "font-weight-bold mt-2";
+
+  heading.appendChild(newExpJob);
+  newExpDivCreated.appendChild(heading);
+  expDiv.appendChild(newExpDivCreated);
+  //-------------------------------------------------------------------------------------------------------
+  var company = document.getElementById("company").value;
+  var newEduCompany = document.createTextNode(company);
+  var subEduCompany = document.createElement("sub");
+
+  subEduCompany.appendChild(newEduCompany);
+  newExpDivCreated.appendChild(subEduCompany);
+  expDiv.appendChild(newExpDivCreated);
+
+  //-------------------------------------------------------------------------------------------------------
+  var expDescription = document.getElementById("expDescription").value;
+  var newExpDescription = document.createTextNode(expDescription);
+  var pExpDescription = document.createElement("p");
+  pExpDescription.className = "mt-4";
+
+  pExpDescription.appendChild(newExpDescription);
+  newExpDivCreated.appendChild(pExpDescription);
+  expDiv.appendChild(newExpDivCreated);
+  //-------------------------------------------------------------------------------------------------------
+  var deleteButton = document.createElement("button");
+
+  deleteButton.innerHTML = "Delete";
+  deleteButton.type = "button";
+  deleteButton.className = "btn btn-danger border-radius mt-1";
+  deleteButton.onclick = handleDelete;
+
+  newExpDivCreated.appendChild(deleteButton);
+  expDiv.appendChild(newExpDivCreated);
+
+  //-------------------------------------------------------------------------------------------------------
+}
+//-------------------------------------------------------------------------------------------------------
 
 function handleDelete(e) {
   var tag = e.target;
   var divCreated = tag.parentNode;
   divCreated.remove();
+
+  // function eduDelete() {
+  //   var div = getElementById("eduBlock");
+  //   div.remove();
+  // }
 }
